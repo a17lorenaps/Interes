42a76986a5a3db9987695f0789cab9e42bfae302
a17lorenaps <lorainaps@hotmail.com>
2018-04-16 12:55:21 +0200

    interes con javadoc

--- src/proyecto_interes/Interes.java
+++ src/proyecto_interes/Interes.java
@@ -1,20 +1,44 @@
-/*
+
+package proyecto_interes;
+
+/**
  * Cálculo de interés simple = capital * redito * tiempo/100.
  * El capital y el tiempo son >0.
  * El redito puede ser negativo o cero.
- * autor: profesor
+ * @author uadmin
+ * @version 1.0.0
  */
-package proyecto_interes;
 public class Interes {
+
+    /**
+     * Clase Interese que ten 3 parametros privados e unha variable publica enteira
+     * x e unha variable enteira que inicializa no valor 2.
+     * os parametos capital e redito son de tipo decimal
+     * tiempo é un parametro de tipo enteiro
+     */
     public int x=2;
     private float capital;
     private float redito;
     private int tiempo;
+
+    /**
+     * Constructor para a clase Interes que asigna valores a la 
+     * capital, redito e tempo
+     * @param capital Capital de tipo decimal
+     * @param redito  Redito de tipo decimal
+     * @param tiempo  tiempo de tipo enteiro
+     */
     public Interes(float capital, float redito, int tiempo) {        
         this.capital=capital;
         this.redito=redito;
         this.tiempo=tiempo;
     }
+
+    /**
+     * Método que permite obter o interes
+     * @return capital*rendito*tiempo de tipo decimal
+     * @throws Exception verifica que o capital e o tempo e maior ou igual que cero, en caso contrario envia un mensaxe
+     */
     public double CalcularInteres()throws Exception{
         if (capital<=0)
             throw (new Exception ("Error. El capital tiene que ser >=0"));
